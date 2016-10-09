cd666ab7d1dab7d47644c78d4fe6780bf00bd90e
Alberto2016 <Alberto2016@2e0fc539-1353-490c-8e16-de71a59953de>
2016-10-09 22:55:01 +0000

    falta atar lo cabos sueltos entre las diferentes pantallas y rellenar la ventana que tienen info en comun entre los pois

--- src/main/webApp/js/pois-domain/colectivo.js
+++ src/main/webApp/js/pois-domain/colectivo.js
@@ -1,47 +1,51 @@
 "use strict";
 function Colectivo(nro) {
-	this.initPoi();
+    this.initPoi();
     this.nroLinea = nro;
     this.paradas = [];
 };
 
 Colectivo.prototype = new POI();
 
-(function() {
-    this.disponible = function() {
+(function () {
+    this.disponible = function () {
         return true;
     };
-    
-    this.getNombre = function() {
+
+    this.getNombre = function () {
         return "Colectivo Nº " + this.nroLinea;
     };
-    
-    this.estaCerca = function(unPunto) {
-    	return this.paradas.some(function(parada){
-    		return parada.estaCerca(unPunto); 
-    	});
-    			
+
+    this.estaCerca = function (unPunto) {
+        return this.paradas.some(function (parada) {
+            return parada.estaCerca(unPunto);
+        });
+
     };
-    
-    this.setParadas = function(_paradas){
-    	this.paradas = _paradas
-	};
-	
-	this.addParada = function(parada) {
-		this.paradas.linea = this;
-		this.paradas.push(parada);
-	};
+
+    this.setParadas = function (_paradas) {
+        this.paradas = _paradas
+    };
+
+    this.addParada = function (parada) {
+        this.paradas.linea = this;
+        this.paradas.push(parada);
+    };
+
+    this.getCantidadDeParadas = function () {
+        paradas.size();
+    }
 
 }).apply(Colectivo.prototype);
 
 
 /* ******************* PARADAS DE COLECTIVO ******************** */
-function ParadaDeColectivo(point) {	
-	this.ubicacion = point;
-	//TODO: radioDeCercania, Redefinirlo en las subCLASES
+function ParadaDeColectivo(point) {
+    this.ubicacion = point;
+    //TODO: radioDeCercania, Redefinirlo en las subCLASES
 };
 
 ParadaDeColectivo.prototype = new POI();
-ParadaDeColectivo.prototype.radioDeCercania = function (){
-	return 0.1;
+ParadaDeColectivo.prototype.radioDeCercania = function () {
+    return 0.1;
 };
